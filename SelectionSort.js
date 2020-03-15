ea44d54e4cc501b22225706e4d649707430d243c
Chandrasekar Arunachalam <kingring23@hotmail.com>
2020-03-15 13:38:03 +0530

    SelectionSort, Initial commit.

---
+++ SelectionSort.js
@@ -0,0 +1,24 @@
+//Selection Sort
+const selectionSort = numberArray => {
+	if (numberArray.length && numberArray.length > 1) {
+		let _numberAray = numberArray.slice();
+		const length = _numberAray.length;
+		let counter = 0;
+		while(counter < length) {
+			let arrayItem = _numberAray[counter],
+				smallestValue = arrayItem,
+				smallestIndex = counter;
+			for(let j = counter; j < length; j++) {
+				if (_numberAray[j] < smallestValue) {
+					smallestValue = _numberAray[j];
+					smallestIndex = j;
+				}
+			}
+			_numberAray[smallestIndex] = arrayItem;
+			_numberAray[counter] = smallestValue;
+			counter += 1;
+		}
+		return _numberAray;
+	}
+	return numberArray;
+}
