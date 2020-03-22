2ca235885800ba38af66657f4031a7a750fa2f4e
Chandrasekar Arunachalam <kingring23@hotmail.com>
2020-03-22 19:14:19 +0530

    Removed typo errors.

--- SelectionSort.js
+++ SelectionSort.js
@@ -1,33 +1,31 @@
 /**
 * Selection Sort
 *
-*/
-
-/**
 * WIP, to feature both ascending &
 * descending order sort.
-*
+* The current algo supports ascending order.
 */
-export const selectionSort = (numberArray, asc = true) => {
-	if (numberArray.length && numberArray.length > 1) {
-		let _numberAray = numberArray.slice();
-		const length = _numberAray.length;
+
+export const selectionSort = array => {
+	if (array.length && array.length > 1) {
+		let _array = array.slice();
+		const length = _array.length;
 		let counter = 0;
-		while(counter < length) {
-			let arrayItem = _numberAray[counter],
+		while (counter < length) {
+			let arrayItem = _array[counter],
 				smallestValue = arrayItem,
 				smallestIndex = counter;
-			for(let j = counter; j < length; j++) {
-				if (_numberAray[j] < smallestValue) {
-					smallestValue = _numberAray[j];
+			for (let j = counter; j < length; j++) {
+				if (_array[j] < smallestValue) {
+					smallestValue = _array[j];
 					smallestIndex = j;
 				}
 			}
-			_numberAray[smallestIndex] = arrayItem;
-			_numberAray[counter] = smallestValue;
+			_array[smallestIndex] = arrayItem;
+			_array[counter] = smallestValue;
 			counter += 1;
 		}
-		return _numberAray;
+		return _array;
 	}
-	return numberArray;
+	return array;
 }
